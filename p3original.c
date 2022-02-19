c2bd3929efdc1527ed17e08ad4f80c5bb345833b
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-02-19 07:19:52 +0000

    Updated P3 Original

---
+++ p3original.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int input_number()
+{
+  int n;
+  printf("Enter Any Number To Check Whether it is Prime or Not\n");
+  scanf("%d",&n);
+  return n;
+}
+int is_prime(int n)
+{
+  int i;
+  for(i=1;i<n/2;i++)
+    {
+      if((n%i)==0)
+        return 1;
+    }
+  return 0;
+}
+void output(int n,int is_prime)
+{
+  if(is_prime==1)
+    printf("%d is Not Prime number\n",n);
+  else
+    printf("%d is  Prime Number \n",n);
+}
+int main()
+{
+  int n,result;
+  n=input_number();
+  result=is_prime(n);
+  output(n,result);
+  return 0;
+}
