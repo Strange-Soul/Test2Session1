5b6b7d17e599a1b902c8a2a3b5956f9b554e7b97
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-02-23 05:54:17 +0000

    P4 Original

--- p4original.c
+++ p4original.c
@@ -11,27 +11,19 @@ int find_fibo(int n)
  int i,fibo;
  int x=0;
   int y=1;
-  for(i=0;i<n;i++)
+  for(i=2;i<n;i++)
     {
-      fibo=y+i-x;
+      fibo=x+y;
       x=y;
       y=fibo;
     }
-   fibo++;
+  
   return fibo;
 }
 void output(int n,int fibo)
 {
-  printf("Fibonacci Series of %d number is \n",n);
-  for(int i=0;i<n-1;i++)
-    {
-      printf("%d \t",i);
-      printf("%d \t",n-1);
-    }
-    
- printf(" Sum of %d Fibonacci Series is: %d \n",n,fibo);
+ printf("Fibonacci series is %d \n",fibo);
 }
- 
 int main()
 {
   int n,result;
