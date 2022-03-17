7eddb21bb94523950273c75c8422a20d2339d4ab
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-03-17 06:50:07 +0000

    p6

--- p6original.c
+++ p6original.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+/*#include<stdio.h>
 
 #include<string.h>
 
@@ -34,5 +34,50 @@ int main()
   char string[20],substring[20];
   input(string,substring);
   printf("Index of substring:%s in string:%s is :%d \n",string,substring,str_index(string,substring));
+  return 0;
+}*/
+
+
+#include<stdio.h>
+#include<string.h>
+
+void input_strings(char *string,char *substring)
+{
+  printf("Enter the String \n");
+  scanf("%s",string);
+  printf("Enter the Substring \n");
+  scanf("%s",substring);
+}
+int str_index(char *string,char *substring)
+{
+  int i=0,j=0,value;
+  while(string[i]!='\0')
+    {
+      while(substring[j]!='\0')
+        {
+          if(substring[j]==string[i])
+          {
+            i++;
+            j++;
+            value=i-strlen(substring);
+          }
+          else
+          {
+            i++;
+          }
+        }
+    }
+  return value;
+}
+void output(char *string,char *substring,int index)
+{
+  printf("Index of Substring:%s in String %s is : %d\n",string,substring,index);
+}
+int main()
+{
+  char s1[30],s2[30];
+  input_strings(s1,s2);
+  int n=str_index(s1,s2);
+  output(s1,s2,n);
   return 0;
 }
