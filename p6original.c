54acaf571da914bf3c591bbbe7b861e9cc116b03
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-03-23 23:58:09 +0000

    P6

--- p6original.c
+++ p6original.c
@@ -11,25 +11,33 @@ int str_index(char *string, char *substring)
   int i=0,j=0,n;
   while(string[i]!='\0')
     {
-      while(string[i]!=substring[0] && string[i]!='\0')
-        n=i;i++;
+     for(;string[i]!='\0' && string[i]!=substring[0];i++)
+       {
+         n=i;
+       }
       if(string[i]=='\0')
-        return 0;
-      while(string[i]==substring[j])
+        return (-1);
+      for(;string[i]==substring[j] && string[i]!='\0';)
         {
           i++;
           j++;
         }
+  if(substring[j]=='\0')
+    return n;
+  if(string[i]=='\0')
+    return -1;
+
+  i++;
       
     }
-  return n;
+  return 0;
 }
 void output(char *string,char *substring,int index)
 {
-  if(index==0)
+  if(index==-1)
     printf("Index of substring : %s in string : %s not found \n",substring,string);
   else
-    printf("Index of substring : %s in String : %s is : %d \n",string,substring,index);
+    printf("Index of substring : %s in String : %s is : %d \n",string,substring,index+1);
 }
 int main()
 {
