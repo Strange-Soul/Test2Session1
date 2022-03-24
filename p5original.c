15afed061a15bccf46c73f0e7d68a8f2e342b428
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-03-24 00:07:37 +0000

    P5

--- p5original.c
+++ p5original.c
@@ -1,5 +1,7 @@
-/*#include<stdio.h>
+#include<stdio.h>
 #include<math.h>
+
+
 int input_array_size()
 {
   int n;
@@ -7,18 +9,15 @@ int input_array_size()
   scanf("%d",&n);
   return n;
 }
-void init_array(int n,int a[n])
+void init_array(int n, int a[n])
 {
   for(int i=0;i<n;i++)
-    {
-      a[i]=i;
-      //a[0]=0;
-      //a[1]=0;
-    }
+    a[i]=i;
+
 }
-void erotosthenes_sieve(int n,int a[n])
+void erotosthenes_sieves(int n, int a[n])
 {
-  int i=2;
+  int i=2;////bec a[0]=0 & a[1]=0 bec not prime
   while(i<sqrt(n))
     {
       while(a[i]==0)
@@ -26,126 +25,19 @@ void erotosthenes_sieve(int n,int a[n])
       for(int j=i+i;j<n;j+=i)
         {
           a[j]=0;
+          a[0]=0;
           a[1]=0;
         }
       i++;
     }
 }
-void output(int n,int a[n])
-{
-  int i;
-  for(i=0;i<n;i++)
-    if(a[i]!='\0')
-    {
-      printf("%d ",a[i]);
-    }
-  printf("\n");
-}
-int main()
-{
-  int x;
-  x=input_array_size();
-  int y[x];
-  init_array(x,y);
-  erotosthenes_sieve(x,y);
-  output(x,y);
-  return 0;
-}
-    */
-
-
-
-/*#include<stdio.h>
-
-
-int input_array_size()
-{
-  int n;
-  printf("Enter the size of array \n");
-  scanf("%d",&n);
-  return n;
-}
-void init_array(int n,int a[n])
-{
-  //1 and 0 are not prime so...?
-  for(int i=0;i<n;i++)
-    {
-      a[i]=i+1;
-    }
-}
-void erotosthenes_sieve(int n,int a[n])
-{
-  int i,j;
-  for(i=1;i<n;i++)
-  {
-   if(a[i]==0)
-    continue;         
-        for(j=a[i]+1;j<=n;j++)//j=2
-          {
-            if(j%a[i]==0)    //2/3!=0
-              a[j-1]=0;
-          }
-   }
-   
-}
-void output(int n,int a[n])
-{ int i;
-  
-    for(int i=0;i<n;i++)
-      {
-        if(a[i]!=0)
-        printf("%d \n",a[i]);
-      
-      }
-}
-int main()
-{
-  int n=input_array_size();
-  int p[n];
-  init_array(n,p);
-  erotosthenes_sieve(n,p);
-  output(n,p);
-  return 0;
-}*/
-
-
-
-//1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,____________
-#include<stdio.h>
-#include<math.h>
-int input_array_size()
-{
-  int n;
-  printf("Enter the size of Array \n");
-  scanf("%d",&n);
-  return n;
-}
-void init_array(int n,int a[n])
+void out_put(int n, int a[n])
 {
   for(int i=0;i<n;i++)
-    a[i]=i;
-}
-void erotosthenes_sieves(int n,int a[n])
-{
-  int j,i=2;
-  while(i<sqrt(n))
     {
-      while(a[i]==0)
-        i++;
-      for(j=i+i;j<n;j+=i)
-        {
-          a[j]=0;
-            a[1]=0;
-        }
-      i++;
+      if(a[i]!=0)
+        printf("%d \t",a[i]);
     }
-}
-void output(int n,int a[n])
-{ for(int i=0;i<n;i++)
-  {
-   if(a[i]!='\0')
-     printf("%d \n",a[i]);
-  }
 }
 int main()
 {
@@ -153,6 +45,8 @@ int main()
   int p[n];
   init_array(n,p);
   erotosthenes_sieves(n,p);
-  output(n,p);
+  out_put(n,p);
   return 0;
 }
+
+
