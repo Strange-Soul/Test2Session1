ce0968b18fe3a011157e01117d43cf904d41901a
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-03-29 16:22:09 +0000

    P2

--- p2original.c
+++ p2original.c
@@ -1,37 +1,36 @@
 #include<stdio.h>
 #include<math.h>
-void input_line(float *x1,float *y1)
+
+void input_line(float *x, float *y)
 {
-  printf("Enter First point (x1,y1)\n");
-  scanf("%f %f",x1,y1);
+  printf("Enter the x and y of point \n");
+  scanf("%f %f",x,y);
 }
-int is_triangle(float x1,float y1,float x2,float y2,float x3,float y3)
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
 {
-   float a,b,c;
-  a=sqrt(pow((x2-x1),2)+pow((y2-y1),2));
-  b=sqrt(pow((x3-x2),2)+pow((y3-y2),2));
-  c=sqrt(pow((x1-x3),2)+pow((y1-y3),2));
-  if((a+b)>c && (b+c)>a && (c+b)>a)
+  float a=sqrt(pow((x2-x1),2)+pow((y2-y1),2));
+  float b=sqrt(pow((x3-x2),2)+pow((y3-y2),2));
+  float c=sqrt(pow((x1-x2),2)+pow((y1-y2),2));
+  if(a+b>c || a+b>c || c+a>b )
     return 1;
   else
     return 0;
 }
-void output(float x1,float y1,float x2,float y2,float x3,float y3 ,int is_triangle)
+void output(float x1, float y1, float x2, float y2,float x3, float y3, int istriangle)
 {
-  if(is_triangle)
-  {
-    printf("Enter points  Form Triangle\n");
-  }
+  if(istriangle==1)
+    printf("Entered points form triangle \n");
   else
-    printf("Enter points  don't form Triangle\n");
+    printf("Entered points don't form triangle \n");
 }
+
 int main()
 {
-  float a1,b1,a2,b2,a3,b3,result;
+  float a1,b1,a2,b2,a3,b3;
   input_line(&a1,&b1);
   input_line(&a2,&b2);
   input_line(&a3,&b3);
-  result=is_triangle(a1,b1,a2,b2,a3,b3);
-  output(a1,b1,a2,b2,a3,b3,result);
+  int r=is_triangle(a1,b1,a2,b2,a3,b3);
+  output(a1,b1,a2,b2,a3,b3,r);
   return 0;
 }
