ad2d321025a8af198b295a3fb6133927322a9b47
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-03-30 04:31:10 +0000

    P3

--- p3original.c
+++ p3original.c
@@ -1,44 +1,40 @@
 #include<stdio.h>
-#include<math.h>
+
 int input_number()
 {
   int n;
-  printf("Enter any Number \n");
+  printf("Enter the number to find prime or not\n");
   scanf("%d",&n);
   return n;
 }
 int is_prime(int n)
 {
-  int p;
-  if(p==0 || p==1)
-  {
-    return p;
-  }
-  for(int i=2;i<sqrt(n);i++)//sqrt(n)=n/sqrt(n);
+  int i;
+  for(i=2;i<n;i++)
     {
-      if((n%i)==0)
+      if(n%i==0)
         return 1;
     }
   return 0;
 }
-void output(int n,int is_prime)
+void output(int n, int is_prime)
 {
   if(n==0 || n==1)
   {
     printf("%d is neither prime nor composite \n",n);
-  }
-  else if(is_prime==1)
+    }
+   else if(is_prime==1)
   {
-    printf("%d is not Prime Number \n",n);
+    printf("%d is a composite number \n",n);
   }
+  
   else
-    printf("%d is Prime Number \n",n);
+    printf("%d is a prime number \n",n);
 }
 int main()
 {
- int n,result;
-  n=input_number();
-  result=is_prime(n);
+  int n=input_number();
+  int result=is_prime(n);
   output(n,result);
   return 0;
 }
